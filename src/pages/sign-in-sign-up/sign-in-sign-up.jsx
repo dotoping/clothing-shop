12ca9aae556f9f44ae23513c453c79f3e61089ce
Daniel <coding.dotping@gmail.com>
2021-06-29 13:41:44 +1000

    final updated signin and signup with Firebase

--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SingOut from '../../components/sign-up/sign-up';
 
 import './sign-in-sign-up.style.scss';
 
 const SignInAndSingUpPage = (props) => (
-    <div className='sign'><SignIn /></div>
+    <div className='sign-in-and-sign-up'><SignIn /><SingOut /></div>
 );
 
 export default SignInAndSingUpPage;
